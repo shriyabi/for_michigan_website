34b3ede419f1850d453cbba1a2f506469601e43a
shriyabi <shriyarbiddala@gmail.coom>
2024-03-20 23:01:18 -0400

    starting on voting information; need to fix the js

--- script.js
+++ script.js
@@ -188,3 +188,18 @@ var slideIndex = 0;
         slides[slideIndex - 1].style.display = "block";
         dots[slideIndex - 1].className += " active";
     }
+
+    function openVote(evt, vote) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(vote).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+    document.getElementById("defaultOpen").click();
